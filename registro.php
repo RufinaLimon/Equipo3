c0859c6844483f9368d404638aece6adad4b8015
Julio Rosaldo <julio.rosaldo@live.com.mx>
2020-10-06 10:34:43 -0500

    Creacion del formulario de registro

---
+++ registro.php
@@ -0,0 +1,9 @@
+<form method="POST" action="registroex.php">
+	Id Usuario: <input type="text" name="idusuario"> <br>
+	Nombre: <input type="text" name="nombre"> <br>
+	Apellido Paterno: <input type="text" name="apellidop"> <br>
+	Apellido Materno: <input type="text" name="apellidom"> <br>
+	Email: <input type="text" name="email"> <br>
+	Contraseña: <input type="text" name="password"> <br>
+	<input type="submit" name="Registrar" value="registrar">
+</form>
