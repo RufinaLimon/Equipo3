45a49cdaa4c05f8d1e80e2c07f7d7d43ed9c9d6e
Julio Rosaldo <julio.rosaldo@live.com.mx>
2020-10-06 11:36:33 -0500

    Mejoro la interfaz de presentacion utilizando bootstrap, por favor, todos utilicen este formato para sus interfaces, gracias

--- registro.php
+++ registro.php
@@ -1,3 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Iluminación</title>
+    <meta charset="UTF-8" name="viewport" content="initial-scale=1">   
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+</head>
+
+<body class="display">    
+    <p></p>
+    
+    <div class="d-flex justify-content-center bg-dark">
+    <div class="btn-group btn-group-lg row" role="group">
+
+    	<a href="#" class="btn btn-primary px-5" role="button">Inicio</a>  
+      
+        <a href="#" class="btn btn-primary px-5" role="button">Movimientos</a>
+
+        <a href="#" class="btn btn-primary px-5" role="button">Transferencias</a>
+
+        <a href="#" class="btn btn-primary px-5" role="button">Precios de criptomonedas</a>              
+       
+        <a href="#" class="btn btn-primary px-5">Salir</a>
+       
+    </div>
+    </div>
+
+    <p></p>
+    
+    <h1>Registro de nuevo usuario</h1>
+
 <form method="POST" action="registroex.php">
 	Id Usuario: <input type="text" name="idusuario"> <br>
 	Nombre: <input type="text" name="nombre"> <br>
@@ -5,5 +39,15 @@
 	Apellido Materno: <input type="text" name="apellidom"> <br>
 	Email: <input type="text" name="email"> <br>
 	Contraseña: <input type="text" name="password"> <br>
-	<input type="submit" name="Registrar" value="registrar">
+	<p></p>
+	<input type="submit" name="registrar" value="Registrar usuario">
 </form>
+
+</body>
+
+<script type="text/javascript" src="light.js"></script>
+<script type="text/javascript" src="jquery-3.3.1.slim.min.js"></script>
+<script type="text/javascript" src="popper.min.js"></script>
+<script type="text/javascript" src="bootstrap.min.js"></script>
+
+</html>
